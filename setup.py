214c69701bf2581c277a877a12fdc20948d125c8
Quentin Augé <quentin.auge@gmail.com>
2020-04-19 15:54:28 +0200

    Initial commit
    Basic project structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+# coding: utf8
+
+from setuptools import find_packages, setup
+
+setup(name='piet',
+      version='0.1',
+      description='Piet playground',
+      author='Quentin Augé',
+      author_email='quentin.auge@gmail.com',
+      license='closed',
+      packages=find_packages(),
+
+      python_requires='>=3.6',
+
+      classifiers=['Programming Language :: Python :: 3 :: Only',
+                   'Operating System :: Unix'],
+
+      extras_require={
+          'testing': ['coverage', 'pytest', 'pytest-cov']
+      },
+
+      entry_points={
+          'console_scripts': [
+              'piet = piet.run:main'
+          ]
+      })
