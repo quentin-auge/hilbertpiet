54789cb243008324ce98f84e7ce7d1c0066001f7
Quentin Augé <quentin.auge@gmail.com>
2020-04-28 19:25:55 +0200

    Improve optimize-piet-numbers cmdline

--- piet/cli/optimize_numbers.py
+++ piet/cli/optimize_numbers.py
@@ -59,20 +59,16 @@ class PushNumberOptimizer:
 
 
 def main():
-    PROJECT_ROOT: Path = Path(__file__).parent.parent
-    DEFAULT_NUMBERS_FILEPATH = PROJECT_ROOT / 'data' / 'numbers.pkl'
-
-    parser = argparse.ArgumentParser('Optimize piet numbers and pickle them')
-    parser.add_argument('--out', type=Path, required=False, default=DEFAULT_NUMBERS_FILEPATH,
-                        help='path to pickle the result to (default: %(default)s)')
-    parser.add_argument('--show-only', action='store_true',
-                        help="open output file and read results, don't optimize neither write")
-    parser.add_argument('max_num', type=int, nargs='?', default=128,
-                        help='largest number to optimize (default: %(default)s)')
+    parser = argparse.ArgumentParser('Optimize and pickle numbers')
+    rw_group = parser.add_mutually_exclusive_group()
+    rw_group.add_argument('--show-only', action='store_true', help='only load and display numbers')
+    parser.add_argument('--limit', type=int, nargs='?', default=128,
+                        help='limit number to optimize (default: %(default)s)')
+    parser.add_argument('filepath', type=Path, help='pickled numbers filepath')
     args = parser.parse_args()
 
     if not args.show_only:
-        opt = PushNumberOptimizer(max_num=args.max_num)
+        opt = PushNumberOptimizer(max_num=args.limit)
 
         print(f'Round 0: cost={opt._cost}')
 
@@ -85,12 +81,12 @@ def main():
             print(f'Round {i}: cost={opt._cost}, {optimize.__name__}')
 
         print()
-        print(f'Saving numbers to {args.out}')
-        opt.save(args.out)
+        print(f'Saving numbers to {args.filepath}')
+        opt.save(args.filepath)
         print()
 
-    PushNumber.load_numbers(args.out)
-    for n in range(1, args.max_num + 1):
+    PushNumber.load_numbers(args.filepath)
+    for n in range(1, args.limit + 1):
         num = PushNumber(n)
         print(f'{n} = {num.decomposition:38} cost={num._cost}')
 
