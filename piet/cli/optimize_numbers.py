f2affad7ab2d75f649fc3ea8a61d0de5c6527f1e
Quentin Augé <quentin.auge@gmail.com>
2020-04-28 15:32:24 +0200

    Add arguments to optimize-piet-numbers cmdline

--- piet/cli/optimize_numbers.py
+++ piet/cli/optimize_numbers.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 from piet.numbers import PushNumber, PushNumberOptimizer
@@ -5,26 +6,36 @@ from piet.numbers import PushNumber, PushNumberOptimizer
 
 def main():
     PROJECT_ROOT: Path = Path(__file__).parent.parent
-    numbers_filepath = PROJECT_ROOT / 'data' / 'numbers.pkl'
+    DEFAULT_NUMBERS_FILEPATH = PROJECT_ROOT / 'data' / 'numbers.pkl'
 
-    opt = PushNumberOptimizer(max_num=128)
+    parser = argparse.ArgumentParser('Optimize piet numbers and pickle them')
+    parser.add_argument('--out', type=Path, required=False, default=DEFAULT_NUMBERS_FILEPATH,
+                        help='path to pickle the result to (default: %(default)s)')
+    parser.add_argument('--show-only', action='store_true',
+                        help="open output file and read results, don't optimize neither write")
+    parser.add_argument('max_num', type=int, nargs='?', default=128,
+                        help='largest number to optimize (default: %(default)s)')
+    args = parser.parse_args()
 
-    print(f'Round 0: cost={opt._cost}')
+    if not args.show_only:
+        opt = PushNumberOptimizer(max_num=args.max_num)
 
-    optimizations = [opt._optimize_pow, opt._optimize_mult, opt._optimize_add] * 2
-    for i, optimize in enumerate(optimizations, 1):
-        optimize()
-        print(f'Round {i}: cost={opt._cost}, {optimize.__name__}')
+        print(f'Round 0: cost={opt._cost}')
 
-    print()
-    print(f'Saving numbers to {numbers_filepath}')
-    opt.save(numbers_filepath)
+        optimizations = [opt._optimize_pow, opt._optimize_mult, opt._optimize_add] * 2
+        for i, optimize in enumerate(optimizations, 1):
+            optimize()
+            print(f'Round {i}: cost={opt._cost}, {optimize.__name__}')
 
-    print()
-    PushNumber.load_numbers(numbers_filepath)
-    for n in range(1, opt.max_num + 2):
+        print()
+        print(f'Saving numbers to {args.out}')
+        opt.save(args.out)
+        print()
+
+    PushNumber.load_numbers(args.out)
+    for n in range(1, args.max_num + 1):
         num = PushNumber(n)
-        print(f'{n} = {num.decomposition:28} cost={num._cost}')
+        print(f'{n} = {num.decomposition:38} cost={num._cost}')
 
 
 if __name__ == '__main__':
