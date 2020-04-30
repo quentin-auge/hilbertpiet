fd347037b7cfcc08fff22e8af36f9abdfe792945
Quentin Augé <quentin.auge@gmail.com>
2020-04-30 18:25:14 +0200

    Introduce `Color` objects
    defined by hue and lightness

---
+++ piet/color.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Color:
+    hue: int
+    lightness: int
+
+    hue_names = ['red', 'yellow', 'green', 'cyan', 'blue', 'magenta']
+    hue_codes = ['#FF0000', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#FF00FF']
+
+    def __init__(self, hue: int, lightness: int):
+        while hue < 0:
+            hue += 6
+        self.hue = hue % 6
+
+        while lightness < 0:
+            lightness += 6
+        self.lightness = lightness % 3
+
+    @classmethod
+    def from_name(cls, name: str) -> Color:
+        if name.startswith('light'):
+            lightness, hue_name = 0, name[len('light'):]
+        elif name.startswith('dark'):
+            lightness, hue_name = 2, name[len('dark'):]
+        else:
+            lightness, hue_name = 1, name
+
+        try:
+            hue = cls.hue_names.index(hue_name)
+        except ValueError:
+            raise ValueError(f'Invalid color name: "{name}"')
+
+        return Color(hue, lightness)
+
+    @property
+    def code(self) -> str:
+
+        hue_code = self.hue_codes[self.hue]
+
+        if self.lightness == 0:
+            code = hue_code.replace('00', 'C0')
+        elif self.lightness == 1:
+            code = hue_code
+        elif self.lightness == 2:
+            code = hue_code.replace('FF', 'C0')
+        else:
+            raise NotImplementedError
+
+        return code
+
+    def __str__(self) -> str:
+        hue_name = self.hue_names[self.hue]
+        if self.lightness == 0:
+            return f'light{hue_name}'
+        elif self.lightness == 1:
+            return hue_name
+        elif self.lightness == 2:
+            return f'dark{hue_name}'
+        else:
+            raise NotImplementedError
---
+++ tests/test_color.py
@@ -0,0 +1,47 @@
+import pytest
+
+from piet.color import Color
+
+hue_lightness_params = [(hue, lightness) for hue in range(7) for lightness in range(4)]
+
+
+@pytest.mark.parametrize('hue, lightness', hue_lightness_params)
+def test_init(hue, lightness):
+    color = Color(hue, lightness)
+    for hue_shift in (0, 6, -6, 12, -12):
+        for lightness_shift in (0, 3, -3, 6, -6):
+            assert Color(hue + hue_shift, lightness + lightness_shift) == color
+
+
+@pytest.mark.parametrize('hue, lightness', hue_lightness_params)
+def test_to_from_name(hue, lightness):
+    color = Color(hue, lightness)
+    actual_color = Color.from_name(str(color))
+    assert color == actual_color
+
+
+invalid_color_names = ['graen', 'vdarkgreen', 'ightgreen', 'lightgreeen', 'darkgren']
+
+
+@pytest.mark.parametrize('invalid_color_name', invalid_color_names)
+def test_invalid_color_name(invalid_color_name):
+    with pytest.raises(ValueError, match='Invalid color name'):
+        color = Color.from_name(invalid_color_name)
+        print(color)
+
+
+color_name_to_codes = [('lightred', '#FFC0C0'), ('lightyellow', '#FFFFC0'),
+                       ('lightgreen', '#C0FFC0'), ('lightcyan', '#C0FFFF'),
+                       ('lightblue', '#C0C0FF'), ('lightmagenta', '#FFC0FF'),
+                       ('red', '#FF0000'), ('yellow', '#FFFF00'), ('green', '#00FF00'),
+                       ('cyan', '#00FFFF'), ('blue', '#0000FF'), ('magenta', '#FF00FF'),
+                       ('darkred', '#C00000'), ('darkyellow', '#C0C000'),
+                       ('darkgreen', '#00C000'), ('darkcyan', '#00C0C0'),
+                       ('darkblue', '#0000C0'), ('darkmagenta', '#C000C0')]
+
+
+@pytest.mark.parametrize('color_name, expected_color_code', color_name_to_codes)
+def test_color_codes(color_name, expected_color_code):
+    color = Color.from_name(color_name)
+    color_code = color.code
+    assert color_code == expected_color_code
