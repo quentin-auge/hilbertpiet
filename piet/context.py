ff09d2fae36a605694998a402c6ca8fb045a3a77
Quentin Augé <quentin.auge@gmail.com>
2020-05-02 17:24:34 +0200

    Introuce directional pointer in `Context`

--- piet/context.py
+++ piet/context.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+
 from typing import List
 
 
@@ -6,14 +7,17 @@ from typing import List
 class Context:
     stack: List[int]
     value: int
+    dp: int
 
-    def __init__(self, stack: List[int] = None, value: int = 0):
+    def __init__(self, stack: List[int] = None, value: int = 0, dp: int = 0):
         self.stack = stack or []
         self.value = value
+        self.dp = dp
 
     def __str__(self):
-        return f'{self.stack} {self.value}'
+        dp_str = ['🡺', '🡻', '🡸', '🡹'][self.dp % 4]
+        return f'{self.stack} {self.value} {dp_str}'
 
     def __deepcopy__(self, _):
-        copy = Context(stack=list(self.stack), value=self.value)
+        copy = Context(stack=list(self.stack), value=self.value, dp=self.dp)
         return copy
--- tests/test_context.py
+++ tests/test_context.py
@@ -1,8 +1,27 @@
 from copy import deepcopy
 
+import pytest
+
 from piet.context import Context
 
 
 def test_str():
     c = Context(stack=[1, 2, 3], value=4)
-    assert str(c) == '[1, 2, 3] 4'
+    assert str(c) == '[1, 2, 3] 4 🡺'
+
+
+@pytest.mark.parametrize('dp,expected_str_dp',
+                         [(0, '🡺'), (1, '🡻'), (2, '🡸'), (3, '🡹'),
+                          (4, '🡺'), (5, '🡻'), (6, '🡸'), (7, '🡹'),
+                          (-4, '🡺'), (-3, '🡻'), (-2, '🡸'), (-1, '🡹')])
+def test_str_dp(dp, expected_str_dp):
+    c = Context(dp=dp)
+    assert str(c) == f'[] 0 {expected_str_dp}'
+
+
+def test_deepcopy():
+    c1 = Context(stack=[1, 2, 3], value=4, dp=2)
+    c2 = deepcopy(c1)
+
+    assert c1 == c2
+    assert c1.stack is not c2.stack
