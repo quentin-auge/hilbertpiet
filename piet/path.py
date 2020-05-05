de717d83cc31791c0a0db29bdce252c43c902150
Quentin Augé <quentin.auge@gmail.com>
2020-05-05 10:49:12 +0200

    Move things around in path.py

--- piet/path.py
+++ piet/path.py
@@ -6,53 +6,6 @@ from piet.macros import Macro
 from piet.ops import Add, Duplicate, Op, Pointer, Pop, Push, Resize
 
 
-def generate_path(n_iterations: int) -> str:
-    """
-    Generate path as a string of turtle instructions to trace a Hilbert curve II after a given
-    number of iterations.
-
-    Notes:
-        Instructions are `F` (forward), `+` (turn right), `-` (turn left).
-        https://elc.github.io/posts/plotting-fractals-step-by-step-with-python/#hilbert-curve-ii.
-    """
-
-    # L-system rules for Hilbert curve II
-    rules = {'X': 'XFYFX+F+YFXFY-F-XFYFX', 'Y': 'YFXFY-F-XFYFX+F+YFXFY'}
-    path = 'X'
-
-    if n_iterations <= 0:
-        return path
-
-    for _ in range(n_iterations):
-        path = ''.join(rules.get(c, c) for c in path)
-
-    path = path.replace('X', '').replace('Y', '')
-
-    return path
-
-
-def stretch_path(path: str, factor: int) -> str:
-    """
-    Stretch a path so that `n` consecutive forwards (`F`) become  `n * factor` consecutive forwards.
-    """
-
-    path += '$'
-    i = 0
-    stretched_path = ''
-    n_forward = 0
-    while i < len(path):
-        c = path[i] if path[i] != '$' else ''
-        if c != 'F':
-            forwards = 'F' * n_forward * factor if n_forward != 1 else 'F'
-            stretched_path += forwards + c
-            n_forward = 0
-        else:
-            n_forward += 1
-        i += 1
-
-    return stretched_path
-
-
 @dataclass(repr=False, eq=False)
 class UTurn(Macro):
     """
@@ -145,3 +98,50 @@ class NoOp(Macro):
         ops.append(Pop())
 
         return ops
+
+
+def generate_path(n_iterations: int) -> str:
+    """
+    Generate path as a string of turtle instructions to trace a Hilbert curve II after a given
+    number of iterations.
+
+    Notes:
+        Instructions are `F` (forward), `+` (turn right + forward), `-` (turn left + forward).
+        https://elc.github.io/posts/plotting-fractals-step-by-step-with-python/#hilbert-curve-ii.
+    """
+
+    # L-system rules for Hilbert curve II
+    rules = {'X': 'XFYFX+F+YFXFY-F-XFYFX', 'Y': 'YFXFY-F-XFYFX+F+YFXFY'}
+    path = 'X'
+
+    if n_iterations <= 0:
+        return path
+
+    for _ in range(n_iterations):
+        path = ''.join(rules.get(c, c) for c in path)
+
+    path = path.replace('X', '').replace('Y', '')
+
+    return path
+
+
+def stretch_path(path: str, factor: int) -> str:
+    """
+    Stretch a path so that `n` consecutive forwards (`F`) become  `n * factor` consecutive forwards.
+    """
+
+    path += '$'
+    i = 0
+    stretched_path = ''
+    n_forward = 0
+    while i < len(path):
+        c = path[i] if path[i] != '$' else ''
+        if c != 'F':
+            forwards = 'F' * n_forward * factor if n_forward != 1 else 'F'
+            stretched_path += forwards + c
+            n_forward = 0
+        else:
+            n_forward += 1
+        i += 1
+
+    return stretched_path
--- tests/test_path.py
+++ tests/test_path.py
@@ -3,6 +3,82 @@ import pytest
 from piet.context import Context
 from piet.path import NoOp, UTurnAntiClockwise, UTurnClockwise, stretch_path
 
+clockwise_params = [pytest.param(True, id='clockwise'), pytest.param(False, id='anticlockwise')]
+
+
+@pytest.mark.parametrize('dp', [1, 1j, -1, -1j])
+@pytest.mark.parametrize('clockwise', clockwise_params)
+def test_uturn(dp, clockwise):
+    original_stack = [2, 20, 3]
+    original_position = 4 + 3j
+    original_dp = dp
+    context = Context(stack=[2, 20, 3], value=1, position=4 + 3j, dp=dp)
+
+    # Initial position and codels number
+    expected_position = original_position
+    expected_size = 0
+
+    # Position and codels number after U-turn setup
+    expected_setup_cost = 3 if clockwise else 5
+    expected_position += expected_setup_cost * dp
+    expected_size += expected_setup_cost
+
+    # Position and codels number after U-turn
+    expected_position += 2 * dp * (1j if clockwise else -1j) - dp
+    expected_size += 3
+
+    # Perform U-turn
+    op = UTurnClockwise() if clockwise else UTurnAntiClockwise()
+    context = op(context)
+
+    assert op.size == expected_size
+
+    # Test resulting context
+
+    assert context.stack == original_stack
+    assert context.value == 1
+    assert context.value == 1
+    assert context.position == expected_position
+    assert context.dp == -original_dp
+    assert context.output == ''
+
+
+@pytest.mark.parametrize('clockwise', clockwise_params)
+def test_uturn_over_invalid_value(clockwise):
+    context = Context(value=3)
+    op = UTurnClockwise() if clockwise else UTurnAntiClockwise()
+    with pytest.raises(RuntimeError, match='Invalid value before U-turn'):
+        print(op(context))
+
+
+@pytest.mark.parametrize('length', [2, 3, 4, 5])
+def test_no_op(length):
+    op = NoOp(length)
+
+    context = Context(stack=[2, 20, 3], value=1, dp=1j)
+
+    context = op(context)
+
+    assert op.size == length
+    assert context.stack == [2, 20, 3]
+    assert context.value == 1
+    assert context.dp == 1j
+    assert context.output == ''
+
+
+@pytest.mark.parametrize('length', [-1, 0, 1])
+def test_no_op_invalid_length(length):
+    with pytest.raises(ValueError, match='Invalid no-op length'):
+        print(NoOp(length))
+
+
+def test_no_op_over_invalid_value():
+    context = Context(value=3)
+    op = NoOp(2)
+    with pytest.raises(RuntimeError, match='Invalid value before no-op'):
+        print(op(context))
+
+
 n_forwards_factors_params = [(n_forwards, factor)
                              for n_forwards in range(4)
                              for factor in range(1, 4)]
@@ -60,80 +136,3 @@ def test_stretch_path_mixed_middle(n_forwards, factor):
     expected_path += '-p-' + 'F' * expected_n_forwards + '+'
 
     assert stretch_path(path, factor) == expected_path
-
-
-clockwise_params = [pytest.param(True, id='clockwise'), pytest.param(False, id='anticlockwise')]
-
-
-@pytest.mark.parametrize('dp', [1, 1j, -1, -1j])
-@pytest.mark.parametrize('clockwise', clockwise_params)
-def test_uturn(dp, clockwise):
-    original_stack = [2, 20, 3]
-    original_position = 4 + 3j
-    original_dp = dp
-    context = Context(stack=[2, 20, 3], value=1, position=4 + 3j, dp=dp)
-
-    # Initial position and codels number
-    expected_position = original_position
-    expected_size = 0
-
-    # Position and codels number after U-turn setup
-    expected_setup_cost = 3 if clockwise else 5
-    expected_position += expected_setup_cost * dp
-    expected_size += expected_setup_cost
-
-    # Position and codels number after U-turn
-    expected_position += 2 * dp * (1j if clockwise else -1j) - dp
-    expected_size += 3
-
-    # Perform U-turn
-    op = UTurnClockwise() if clockwise else UTurnAntiClockwise()
-    context = op(context)
-
-    assert op.size == expected_size
-
-    # Test resulting context
-
-    assert context.stack == original_stack
-    assert context.value == 1
-    assert context.value == 1
-    assert context.position == expected_position
-    assert context.dp == -original_dp
-    assert context.output == ''
-
-
-
-@pytest.mark.parametrize('clockwise', clockwise_params)
-def test_uturn_over_invalid_value(clockwise):
-    context = Context(value=3)
-    op = UTurnClockwise() if clockwise else UTurnAntiClockwise()
-    with pytest.raises(RuntimeError, match='Invalid value before U-turn'):
-        print(op(context))
-
-
-@pytest.mark.parametrize('length', [2, 3, 4, 5])
-def test_no_op(length):
-    op = NoOp(length)
-
-    context = Context(stack=[2, 20, 3], value=1, dp=1j)
-
-    context = op(context)
-
-    assert op.size == length
-    assert context.stack == [2, 20, 3]
-    assert context.value == 1
-    assert context.dp == 1j
-    assert context.output == ''
-
-
-@pytest.mark.parametrize('length', [-1, 0, 1])
-def test_no_op_invalid_length(length):
-    with pytest.raises(ValueError, match='Invalid no-op length'):
-        print(NoOp(length))
-
-
-def test_no_op_over_invalid_value():
-    context = Context(value=3)
-    op = NoOp(2)
-    with pytest.raises(RuntimeError, match='Invalid value before no-op'):
-        print(op(context))
