1354063e54acc42ff5f36f6c370947c936463b8b
Quentin Augé <quentin.auge@gmail.com>
2020-05-05 02:45:50 +0200

    Introduce `UTurn` macro

--- piet/path.py
+++ piet/path.py
@@ -1,3 +1,11 @@
+from dataclasses import dataclass
+from typing import List
+
+from piet.context import Context
+from piet.macros import Macro
+from piet.ops import Duplicate, Op, Pointer, Pop, Push, Resize
+
+
 def generate_path(n_iterations: int) -> str:
     """
     Generate path as a string of turtle instructions to trace a Hilbert curve II after a given
@@ -43,3 +51,45 @@ def stretch_path(path: str, factor: int) -> str:
         i += 1
 
     return stretched_path
+
+
+@dataclass
+class UTurn(Macro):
+    """
+    Perform a U-turn with codels. Stack remains the same.
+
+    The operations are the following:
+        * Setup:
+            * Clockwise: 3 codels in the direction of dp
+            * Anticlockwise: 3 codels in the direction of dp
+            * Optional: `padding` extra codels if need be
+        * First dp rotation: 1 codel
+        * Move forward: 1 codel
+        * Second dp rotation: 1 codel
+
+    Notes:
+        It is assumed the last codel before U-turn is anything but a `Resize` operation.
+    """
+
+    clockwise: bool
+    padding: int = 0
+
+    def __call__(self, context: Context) -> Context:
+
+        if context.value != 1:
+            raise RuntimeError(f'Invalid value before U-turn: {context.value}')
+
+        return super().__call__(context)
+
+    @property
+    def ops(self) -> List[Op]:
+
+        ops = [Push(), Duplicate(), Duplicate(), Pointer(), Pop(), Pointer()]
+
+        if self.padding:
+            ops.insert(1, Resize(self.padding + 1))
+
+        if not self.clockwise:
+            ops = [Resize(3)] + ops
+
+        return ops
--- tests/test_path.py
+++ tests/test_path.py
@@ -1,6 +1,7 @@
 import pytest
 
-from piet.path import stretch_path
+from piet.context import Context
+from piet.path import UTurn, stretch_path
 
 n_forwards_factors_params = [(n_forwards, factor)
                              for n_forwards in range(4)
@@ -59,3 +60,55 @@ def test_stretch_path_mixed_middle(n_forwards, factor):
     expected_path += '-p-' + 'F' * expected_n_forwards + '+'
 
     assert stretch_path(path, factor) == expected_path
+
+
+@pytest.mark.parametrize('dp', [1, 1j, -1, -1j])
+@pytest.mark.parametrize('clockwise', [True, False])
+@pytest.mark.parametrize('padding', list(range(4)))
+def test_uturn(dp, clockwise, padding):
+    original_stack = [2, 20, 3]
+    original_position = 4 + 3j
+    original_dp = dp
+    context = Context(stack=[2, 20, 3], value=1, position=4 + 3j, dp=dp)
+
+    # Initial position and codels number
+    expected_position = original_position
+    expected_size = 0
+
+    # Position and codels number after clockwise / Anticlockwise U-turn setup
+    if not clockwise:
+        expected_position += 2 * dp
+        expected_size += 2
+
+    # Position and codels number after U-turn setup
+    expected_position += 3 * dp
+    expected_size += 3
+
+    # Position and codels number after padding
+    expected_position += padding * dp
+    expected_size += padding
+
+    # Position and codels number after U-turn
+    expected_position += 2 * dp * (1j if clockwise else -1j) - dp
+    expected_size += 3
+
+    # Perform U-turn
+    op = UTurn(clockwise, padding)
+    context = op(context)
+
+    # Test resulting context
+    assert op.size == expected_size
+
+    assert context.stack == original_stack
+    assert context.value == 1
+    assert context.value == 1
+    assert context.position == expected_position
+
+    assert context.dp == -original_dp
+
+
+def test_uturn_over_invalid_value():
+    context = Context(value=3)
+    op = UTurn(clockwise=True)
+    with pytest.raises(RuntimeError, match='Invalid value before U-turn'):
+        print(op(context))
